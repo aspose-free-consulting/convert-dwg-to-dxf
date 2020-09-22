d97a116c4e9eb724e39b2305b6d55230b30b9f78
mudassirfayyaz <mudassir.fayyaz@aspose.com>
2020-09-22 12:52:19 +0500

    New project added

---
+++ DWGToEDXF/Program.cs
@@ -0,0 +1,52 @@
+﻿using Aspose.CAD;
+using Aspose.CAD.FileFormats.Cad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWGToEDXF
+{
+    class Program
+    {
+        public static void LoadLicense()
+        {
+
+            try
+            {
+                String licPath = @"C:\Aspose Data\Aspose.Total.Product.Family.lic";
+
+                Aspose.CAD.License lic = new Aspose.CAD.License();
+                lic.SetLicense(licPath);
+            }
+            catch(Exception e)
+            {
+                String s = e.StackTrace;
+            }
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            LoadLicense();
+
+            try
+            {
+                string inputFile = "Line.dwg";
+                string outFile = "Line.dxf";
+                using (var cadImage = (CadImage)Image.Load(inputFile))
+                {
+                    cadImage.Save(outFile);
+                }
+            }
+            catch(Exception ex)
+            {
+
+                String Message = ex.StackTrace;
+            }
+
+
+        }
+    }
+}
